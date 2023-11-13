d99879820efe2284f3c4ee01de0d04bb08ad77c1
Erin Shaben <eshaben@icloud.com>
2023-11-13 17:19:07 -0500

    add tests for the xc20 overview page

---
+++ test/builders/interoperability/xcm/xc20/overview/retrieve-xc20s.js
@@ -0,0 +1,48 @@
+import { assert } from 'chai';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+
+describe('Overview of XC-20s - Current List of External XC-20s', function () {
+  const getApi = async (wssEndpoint) => {
+    const wsProvider = new WsProvider(wssEndpoint);
+    const api = await ApiPromise.create({ provider: wsProvider, noInitWarn: true });
+
+    return api;
+  };
+
+  describe('Retrieve List of External XC-20s and Their Metadata', async () => {
+    /** These tests will let us know when we need to update the list of XC-20s.
+     * So we are hardcoding in the current number and any time this test fails,
+     * we'll know we need to update the docs along with this test.
+     */
+    it('should return the list of XC-20s on Moonbeam', async () => {
+      const api = await getApi('wss://wss.api.moonbeam.network');
+
+      const assets = await api.query.assets.asset.entries();
+      assert.equal(assets.length, 26n);
+
+      api.disconnect();
+    });
+
+    it('should return the list of XC-20s on Moonriver', async () => {
+      const api = await getApi('wss://wss.api.moonriver.moonbeam.network');
+
+      const assets = await api.query.assets.asset.entries();
+      assert.equal(assets.length, 23n);
+
+      api.disconnect();
+    });
+
+    /** This test is to ensure we can access the metadata as expected */
+    it('should return the metadata for an asset', async () => {
+      const api = await getApi('wss://wss.api.moonbeam.network');
+      const xcDOT = 42259045809535163221576417993425387648n;
+
+      const metadata = (await api.query.assets.metadata(xcDOT)).toHuman();
+      assert.equal(metadata.name, 'xcDOT');
+      assert.equal(metadata.symbol, 'xcDOT');
+      assert.equal(metadata.decimals, '10');
+
+      api.disconnect();
+    });
+  });
+});
--- test/run_python_tests.py
+++ test/run_python_tests.py
@@ -1,14 +1,25 @@
 import unittest
 import sys
+import os
 
 loader = unittest.TestLoader()
-web3py_tests = loader.discover("test/builders/build/eth-api/libraries/web3py", pattern="test_*.py")
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+web3py_tests = loader.discover(
+    "test/builders/build/eth-api/libraries/web3py", top_level_dir=dir_path
+)
+web3py_xc20_overview_local_xc20_tests = loader.discover(
+    "test/builders/interoperability/xcm/xc20/overview/local-xc20s",
+    top_level_dir=dir_path,
+)
+
 
 def suite():
     testSuite = unittest.TestSuite()
     testSuite.addTest(web3py_tests)
+    testSuite.addTest(web3py_xc20_overview_local_xc20_tests)
     return testSuite
+
 
 if __name__ == "__main__":
     result = unittest.TextTestRunner(verbosity=2).run(suite())
-    sys.exit(not result.wasSuccessful())
