00acf111245f65801631ff9ac12aeafb9f6fbfa7
Erin Shaben <eshaben@icloud.com>
2024-04-08 10:31:51 -0400

    bump moonriver xc-20 number for xcTNKR

--- test/builders/interoperability/xcm/xc20/overview/retrieve-xc20s.js
+++ test/builders/interoperability/xcm/xc20/overview/retrieve-xc20s.js
@@ -27,7 +27,7 @@ describe('Overview of XC-20s - Current List of External XC-20s', function () {
       const api = await getApi('wss://wss.api.moonriver.moonbeam.network');
 
       const assets = await api.query.assets.asset.entries();
-      assert.equal(assets.length, 23n);
+      assert.equal(assets.length, 24n);
 
       api.disconnect();
     }).timeout(15000);
