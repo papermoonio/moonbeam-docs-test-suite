9854c366afb0e506932f53c417f0a42d20ce2c7c
Kevin Neilson <kevin@papermoon.io>
2025-11-05 14:37:48 -0800

    runtime upgrades and account balances

--- test/builders/get-started/eth-compare/account-balances.js
+++ test/builders/get-started/eth-compare/account-balances.js
@@ -1,7 +1,6 @@
 import { assert, expect } from 'chai';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { ethers } from 'ethers';
-import { stringToHex } from '@polkadot/util';
 
 describe('Account Balances', () => {
   const alice = {
@@ -19,26 +18,21 @@ describe('Account Balances', () => {
     await api.isReady;
   });
 
-  describe('Moonbeam Account Balances - Retrieve Your Balance', async () => {
-    it('should return the balance of an account', async () => {
-      // Create new account
-      const bob = ethers.Wallet.createRandom().address;
-      // Get the balance of the new account
-      const balanceData = await api.query.balances.account(bob);
-      // Bob's initial balance should be 0
-      const freeBalance = balanceData.toJSON().free;
-      assert.equal(freeBalance, 0);
+  describe('Moonbeam Account Balances - Retrieve Your Balance', () => {
+    it('should return the balance of a new account as 0', async () => {
+      const bob = ethers.Wallet.createRandom().address; // H160 works on Moonbeam
+      const { free } = await api.query.balances.account(bob);
+      assert.isTrue(free.isZero(), 'New accounts should start with 0 free balance');
     });
-    it('should return the balance locks of an account', async () => {
-      // Get the balance locks for Alice
-      const locksData = await api.query.balances.locks(alice.address);
-      // Convert to JSON to access the data
-      const locksDataJson = locksData.toJSON();
-      // Alice should have one balance lock since she is the default collator on dev nodes
-      const collatorLock = locksDataJson[0];
-      const lockId = stringToHex('stkngcol');
-      // Assert that she has a balance lock for collating
-      assert.equal(lockId, collatorLock.id);
+  
+    it('should show a ParachainStaking::StakingCollator freeze for Alice', async () => {
+      const freezes = await api.query.balances.freezes(alice.address); // Vec<BalanceFreeze>
+      assert.isAtLeast(freezes.length, 1, 'Alice should have at least one freeze');
+  
+      const hasCollatorFreeze = freezes.some(
+        f => f.id.isParachainStaking && f.id.asParachainStaking.isStakingCollator
+      );
+      assert.isTrue(hasCollatorFreeze, 'Expected a ParachainStaking::StakingCollator freeze');
     });
   });
 
